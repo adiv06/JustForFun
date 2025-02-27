77214f751c8a51ca72e8c3e147a269ad1117a48e
Adiv Padgilwar <adivp432@gmail.com>
2025-02-26 19:38:42 -0600

    Initial Commit

---
+++ GoodEats/GoodEatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodEatsApp.swift
+//  GoodEats
+//
+//  Created by Padgilwar, Adiv on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct GoodEatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GoodEatsTests/GoodEatsTests.swift
@@ -0,0 +1,16 @@
+//
+//  GoodEatsTests.swift
+//  GoodEatsTests
+//
+//  Created by Padgilwar, Adiv on 2/26/25.
+//
+
+import Testing
+
+struct GoodEatsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
